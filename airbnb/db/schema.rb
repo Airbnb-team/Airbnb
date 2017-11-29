7e0af30567e0e14f8bc197e5acdb79492e0a8d64
Masayuki <o8o95006237@gmail.com>
2017-11-29 14:23:33 +0900

    photoテーブル作成
    photo table create

--- airbnb/db/schema.rb
+++ airbnb/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171129051351) do
+ActiveRecord::Schema.define(version: 20171129052051) do
 
   create_table "favorites", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id",    null: false
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 20171129051351) do
   end
 
   create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "photos", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "user_id",    null: false
+    t.integer  "room_id",    null: false
+    t.string   "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
