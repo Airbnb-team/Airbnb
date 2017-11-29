ed8a5fa8b460651554b8c2da34e7568004307165
Masayuki <o8o95006237@gmail.com>
2017-11-29 14:42:46 +0900

    amenityテーブル作成
    amenity table create

--- airbnb/db/schema.rb
+++ airbnb/db/schema.rb
@@ -10,7 +10,59 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171129052051) do
+ActiveRecord::Schema.define(version: 20171129052351) do
+
+  create_table "amenities", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.boolean  "pet"
+    t.boolean  "elevator"
+    t.boolean  "kid_friendly"
+    t.boolean  "gym"
+    t.boolean  "doorman"
+    t.boolean  "breakfast"
+    t.boolean  "suitable_for_events"
+    t.boolean  "free_parking"
+    t.boolean  "smoking_allowed"
+    t.boolean  "wheelchair_accessible"
+    t.boolean  "wireless_internet"
+    t.boolean  "hot_tub"
+    t.boolean  "air_conditioning"
+    t.boolean  "kitchen"
+    t.boolean  "cable_tv"
+    t.boolean  "wireless_intercom"
+    t.boolean  "baby_bath"
+    t.boolean  "baby_monitor"
+    t.boolean  "babysitter"
+    t.boolean  "bathtub"
+    t.boolean  "changing_table"
+    t.boolean  "childrend_books_and_toys"
+    t.boolean  "childrens_dinnerware"
+    t.boolean  "crib"
+    t.boolean  "fireplace_guards"
+    t.boolean  "indoor_fireplace"
+    t.boolean  "pool"
+    t.boolean  "shampoo"
+    t.boolean  "hangers"
+    t.boolean  "iron"
+    t.boolean  "dryer"
+    t.boolean  "hair_dryer"
+    t.boolean  "Laptop"
+    t.boolean  "tv"
+    t.boolean  "washer"
+    t.boolean  "heating"
+    t.boolean  "free_parking_on_street"
+    t.boolean  "private_entrance"
+    t.boolean  "ethernet_connection"
+    t.boolean  "game_console"
+    t.boolean  "high_chair"
+    t.boolean  "outlet_covers"
+    t.boolean  "travel_crib"
+    t.boolean  "room_darkening_shades"
+    t.boolean  "stair_gates"
+    t.boolean  "estable_corner_guardssentials"
+    t.boolean  "table_corner_guards"
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
+  end
 
   create_table "favorites", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id",    null: false
