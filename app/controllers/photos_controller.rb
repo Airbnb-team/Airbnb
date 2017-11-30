7bb2ec9e32a000a37c2483a98889b49bb0787595
Satoru <satoru.hanada0410@gmail.com>
2017-11-30 16:38:23 +0900

    RE_VIEW

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,19 @@
+class PhotosController < ApplicationController
+
+	def create
+		@photo = Photo.create(set_room)
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	private
+
+	def set_room
+		@photo = Photo.find_by(params[:id])
+	end
+	
+end
