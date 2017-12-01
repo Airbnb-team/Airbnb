40b8b41da167949ad6a9243922cd91e2aa8e8a84
Masayuki <o8o95006237@gmail.com>
2017-12-01 11:31:56 +0900

    コンフリクト解消
    index先をscreenに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'search#index'
+  root 'screen#index'
   resource :users, only: [:new, :create]
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 
