f9eadbe8aa900cc1524a7e295795e3ec3b57c95f
yamazaki8934 <j6145.yamazaki.4232@outlook.jp>
2017-12-01 12:47:21 +0900

    Commentout-facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,27 +8,28 @@ class User < ApplicationRecord
   has_many :favarites
   has_many :reviews
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:facebook]
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+         # , omniauth_providers: [:facebook]
 
-   # userがいなければfacebookアカウントでuserを作成するメソッド
-   def self.from_omniauth(auth)
-     user = User.where(email: auth.info.email).first
-     if user
-       return user
-     else
-       where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-         # userモデルが持っているカラムをそれぞれ定義していく
-         user.email = auth.info.email
-         user.password = Devise.friendly_token[0,20]
-         user.name = auth.info.name
-         user.image = auth.info.image
-         user.uid = auth.uid
-         user.provider = auth.provider
-
-         # If you are using confirmable and the provider(s) you use validate emails,
-         # uncomment the line below to skip the confirmation emails.
-        #  user.skip_confirmation!
-       end
-     end
-   end
+ #   # userがいなければfacebookアカウントでuserを作成するメソッド
+ #   def self.from_omniauth(auth)
+ #     user = User.where(email: auth.info.email).first
+ #     if user
+ #       return user
+ #     else
+ #       where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+ #         # userモデルが持っているカラムをそれぞれ定義していく
+ #         user.email = auth.info.email
+ #         user.password = Devise.friendly_token[0,20]
+ #         user.name = auth.info.name
+ #         user.image = auth.info.image
+ #         user.uid = auth.uid
+ #         user.provider = auth.provider
+ #
+ #         # If you are using confirmable and the provider(s) you use validate emails,
+ #         # uncomment the line below to skip the confirmation emails.
+ #        #  user.skip_confirmation!
+ #       end
+ #     end
+ #   end
  end
