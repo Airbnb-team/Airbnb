1b6c6adc2132a3e813e15be83c754ce50f2caaf8
yamazaki8934 <j6145.yamazaki.4232@outlook.jp>
2017-12-01 12:50:28 +0900

    Comment out facebook2

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'screen#index'
   resource :users, only: [:new, :create]
-  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
+  devise_for :users
+  # , controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 
   get 'screen' => 'screen#host'
 end
