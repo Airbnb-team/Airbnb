c5f97db7d7d125dcc165f0b543613103ef9f5b7f
Masayuki <o8o95006237@gmail.com>
2017-12-04 10:31:08 +0900

    ホスト検索結果画面作成
    ホスト検索画面の作成。

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'screen#index'
-  resource :users, only: [:new, :create]
-  resource :rooms, only: [:index, :create, :new, :edit, :show, :update]
+  resources :users, only: [:new, :create]
+  resources :rooms
   devise_for :users
   # , controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 end
