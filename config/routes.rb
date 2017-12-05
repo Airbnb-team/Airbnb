d4ecc019001165344119f0fed11485a4c690f627
Satoru <satoru.hanada0410@gmail.com>
2017-12-05 11:24:30 +0900

    Modify2

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,6 @@ Rails.application.routes.draw do
   root 'screen#index'
   devise_for :users
   resources :users, only: [:index, :edit, :update, :new, :create]
-  resources :rooms, [:index, :create, :new, :edit, :show, :update] do
-		resources :bedrooms, only: [:create, :edit, :update]
-		resources :bathrooms, only: [:create, :edit, :update]
-		resources :locations, only: [:create, :edit, :update]
-    resources :locationmaps, only: [:create, :edit, :update]
-    resources :amenities, only: [:create, :edit, :update]
-    resources :spaces, only: [:create, :edit, :update]
-	end
+  resources :rooms
   # , controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 end
