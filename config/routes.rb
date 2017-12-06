98fe65f4f37b5fc9980e9ea13ef2bfb4239366eb
Masayuki <o8o95006237@gmail.com>
2017-12-06 10:32:45 +0900

    コンフリクト解消
    コンフリクト解消

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'screen#index'
-<<<<<<< HEAD
-  resources :users, only: [:new, :create]
+  resources :users
   resources :rooms
-  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
-=======
-  devise_for :users
-  resources :users, only: [:index, :edit, :update, :new, :create]
-  resources :rooms, only: [:index, :new, :create, :edit, :show, :update] do
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" } do
     collection do
       get 'continue'
     end
@@ -25,5 +20,4 @@ Rails.application.routes.draw do
 	end
   # , controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
   get 'screen' => 'screen#photo'
->>>>>>> master
 end
