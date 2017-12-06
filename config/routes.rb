4f76f47a6b5673c2239818cfab040a0c71ee732f
Masayuki <o8o95006237@gmail.com>
2017-12-06 10:09:34 +0900

    ホストのedit画面を作成
    ホストのedit画面を作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'screen#index'
-  devise_for :users
   resources :users, only: [:new, :create]
   resources :rooms
-  # , controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 end
