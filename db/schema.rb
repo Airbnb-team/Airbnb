40a374ee384248d862f134649126631bb1602eef
yamazaki8934 <j6145.yamazaki.4232@outlook.jp>
2017-12-12 11:03:53 +0900

    Construct database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171208080144) do
+ActiveRecord::Schema.define(version: 20171212020022) do
 
   create_table "Explanatories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "title",                              null: false
@@ -111,6 +111,13 @@ ActiveRecord::Schema.define(version: 20171208080144) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "user_id"
+    t.integer  "message_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "locationmaps", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "room_id",     null: false
     t.datetime "created_at",  null: false
@@ -139,6 +146,9 @@ ActiveRecord::Schema.define(version: 20171208080144) do
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
     t.text     "body",       limit: 65535
+    t.integer  "user_id"
+    t.integer  "group_id"
+    t.string   "image"
   end
 
   create_table "photos", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -150,12 +160,13 @@ ActiveRecord::Schema.define(version: 20171208080144) do
   end
 
   create_table "reservations", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer  "user_id",    null: false
-    t.integer  "room_id",    null: false
-    t.date     "check_in",   null: false
-    t.date     "check_out",  null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "user_id",     null: false
+    t.integer  "room_id",     null: false
+    t.date     "check_in",    null: false
+    t.date     "check_out",   null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "guest_count"
   end
 
   create_table "reviews", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -213,6 +224,15 @@ ActiveRecord::Schema.define(version: 20171208080144) do
     t.datetime "updated_at",                 null: false
   end
 
+  create_table "user_groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "user_id"
+    t.integer  "group_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["group_id"], name: "index_user_groups_on_group_id", using: :btree
+    t.index ["user_id"], name: "index_user_groups_on_user_id", using: :btree
+  end
+
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email",                                default: "", null: false
     t.string   "encrypted_password",                   default: "", null: false
@@ -236,6 +256,7 @@ ActiveRecord::Schema.define(version: 20171208080144) do
     t.string   "image"
     t.string   "provider"
     t.string   "uid"
+    t.integer  "group_id"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
@@ -251,4 +272,6 @@ ActiveRecord::Schema.define(version: 20171208080144) do
   add_foreign_key "locations", "users"
   add_foreign_key "rules", "rooms"
   add_foreign_key "rules", "users"
+  add_foreign_key "user_groups", "groups"
+  add_foreign_key "user_groups", "users"
 end
