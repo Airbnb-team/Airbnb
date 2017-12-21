283999c3ddee46d53452b363f2d73342af7f7f45
yamazaki8934 <j6145.yamazaki.4232@outlook.jp>
2017-12-21 17:05:54 +0900

    Improve signin and signet

--- app/assets/javascripts/group_index.js
+++ app/assets/javascripts/group_index.js
@@ -1,29 +1,29 @@
-$(function() {
-var tabs = document.getElementById('tabcontrol').getElementsByTagName('a');
-
-var pages = document.getElementById('tabbody').getElementsByTagName('div');
-
-for(var i=0; i<tabs.length; i++) {
-   tabs[i].onclick = changeTab;
-}
-
-function changeTab() {
-   var targetid = this.href.substring(this.href.indexOf('#')+1,this.href.length);
-   for(var i=0; i<pages.length; i++) {
-      if( pages[i].id != targetid ) {
-         pages[i].style.display = "none";
-      }
-      else {
-         pages[i].style.display = "block";
-      }
-   }
-   for(var i=0; i<tabs.length; i++) {
-      tabs[i].style.zIndex = "0";
-   }
-   this.style.zIndex = "10";
-   return false;
-}
-
-tabs[0].onclick();
-
-});
+// $(function() {
+// var tabs = document.getElementById('tabcontrol').getElementsByTagName('a');
+//
+// var pages = document.getElementById('tabbody').getElementsByTagName('div');
+//
+// for(var i=0; i<tabs.length; i++) {
+//    tabs[i].onclick = changeTab;
+// }
+//
+// function changeTab() {
+//    var targetid = this.href.substring(this.href.indexOf('#')+1,this.href.length);
+//    for(var i=0; i<pages.length; i++) {
+//       if( pages[i].id != targetid ) {
+//          pages[i].style.display = "none";
+//       }
+//       else {
+//          pages[i].style.display = "block";
+//       }
+//    }
+//    for(var i=0; i<tabs.length; i++) {
+//       tabs[i].style.zIndex = "0";
+//    }
+//    this.style.zIndex = "10";
+//    return false;
+// }
+//
+// tabs[0].onclick();
+//
+// });
