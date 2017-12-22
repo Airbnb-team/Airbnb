8cbce583dbe7984b2f4723a8a50a0cfb7840b9c5
yamazaki8934 <j6145.yamazaki.4232@outlook.jp>
2017-12-22 20:34:24 +0900

    fix typo

--- db/migrate/20171207085128_change_home_type_to_rooms.rb
+++ db/migrate/20171207085128_change_home_type_to_rooms.rb
@@ -1,9 +1,11 @@
 class ChangeHomeTypeToRooms < ActiveRecord::Migration[5.0]
   def up
-    change_column :Rooms, :home_type, :string, null: false, default: 0
+    # change_column :Rooms, :home_type, :string, null: false, default: 0
+    change_column :rooms, :home_type, :string, null: false, default: 0
   end
 
   def down
-    change_column :Rooms, :home_type, :intger, null: true
+    # change_column :Rooms, :home_type, :intger, null: true
+    change_column :rooms, :home_type, :intger, null: true
   end
 end
